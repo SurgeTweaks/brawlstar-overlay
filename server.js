7bbe84f943943568b949c5c706f066c79cbbc02a
cytra <luka.constant@hotmail.com>
2025-06-04 15:08:26 +0200

    update

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const { getPlayerStats } = require('./api/brawlstars');
 const { port } = require('./config');
 
 const app = express();
+const fs = require('fs');
+const TAG_FILE = path.join(__dirname, 'tag.json');
+
+// GET tag
+app.get('/api/tag', (req, res) => {
+  const data = fs.readFileSync(TAG_FILE, 'utf-8');
+  const tag = JSON.parse(data).tag;
+  res.json({ tag });
+});
+
+// POST tag (depuis admin)
+app.use(express.json());
+app.post('/api/tag', (req, res) => {
+  const newTag = req.body.tag;
+  if (!newTag || typeof newTag !== 'string') {
+    return res.status(400).json({ error: 'Tag invalide' });
+  }
+
+  fs.writeFileSync(TAG_FILE, JSON.stringify({ tag: newTag }));
+  res.json({ success: true, tag: newTag });
+});
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
